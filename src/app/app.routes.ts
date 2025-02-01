e4318fd52a6a57346b44d135d5d84eace6e11784
Diego <die.moralesh@gmail.com>
2025-02-01 10:17:59 -0300

    add service pokemon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,32 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'pokemons',
+    loadComponent: () => import('./pages/pokemons/pokemons-page.component'),
+  },
+  {
+    path: 'pokemons/:id',
+    loadComponent: () => import('./pages/pokemon/pokemon-page.component'),
+  },
+  {
     path: 'about',
-    loadComponent: () => import('./pages/about/about-page.component')
+    loadComponent: () => import('./pages/about/about-page.component'),
   },
   {
     path: 'pricing',
-    loadComponent: () => import('./pages/pricing/pricing-page.component')
+    loadComponent: () => import('./pages/pricing/pricing-page.component'),
   },
   {
     path: 'contact',
-    loadComponent: () => import('./pages/contact/contact-page.component')
+    loadComponent: () => import('./pages/contact/contact-page.component'),
   },
+
   {
     path: '**',
-    redirectTo: 'about'
+    redirectTo: () => {
+      // const authService = inject(AuthService)
+
+      return 'about';
+    },
   },
 ];
