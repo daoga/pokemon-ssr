82a5f0d2495e1aea46d7d3353b60c0b80b9936dc
Diego <die.moralesh@gmail.com>
2025-02-01 11:04:19 -0300

    add nuevos estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn .2s ease-in-out'
+      }
+    },
   },
   plugins: [],
 }
